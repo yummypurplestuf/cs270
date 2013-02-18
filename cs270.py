bbb1a07d2e471684a0a69a1407170706c7ee07e3
Jared R. Luellen <Jared.Luellen@cs.olivetcollege.edu>
2013-02-18 11:58:56 -0500

    trying to fix some Heroku stuff

--- cs270.py
+++ cs270.py
@@ -1,33 +1,23 @@
 import os
 from flask import Flask, redirect, url_for
 
-# A Flask app instance
 app = Flask(__name__)
 
-
-# Route requests to the top level... say Hello
-
-
+@app.route('/')
+def hello():
+    return redirect(url_for('static', filename='index.html'))
 
 # Assignment 1: A haphazard page showing the most common HTML tags
-@app.route('/', '/assignment-1', '/assignment-2', '/assignment-3')
-
-def index():
-    return redirect(url_for('/', filename='/'))
-
+@app.route('/assignment1')
 def assignment_1():
     return redirect(url_for('static', filename='assignment-1.html'))
-    <a href="/static/assignment-1.html">assignment-1</a>
+# Assignment 1: A haphazard page showing the most common HTML tags
+@app.route('/assignment3')
+def assignment_3():
+    return redirect(url_for('static', filename='assignment-3.html'))
 
-def assignment_1():
-    return redirect(url_for('static', filename='assignment-1.html'))
-    <a href="/static/assignment-2.html">assignment-2</a>
 
-def assignment_1():
-    return redirect(url_for('static', filename='assignment-1.html'))
-    <a href="/static/assignment-3.html">assignment-3</a>
-
-# A module runner to make our app go!
 if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
