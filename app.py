56d0c98bdd5bfa2c460ebada8f07b0166f5c25bf
Jared R. Luellen <Jared.Luellen@cs.olivetcollege.edu>
2013-02-18 11:56:38 -0500

    trying to fix some Heroku stuff

--- app.py
+++ app.py
@@ -7,26 +7,17 @@ app = Flask(__name__)
 def hello():
     return redirect(url_for('static', filename='index.html'))
 
-if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
-
-
 # Assignment 1: A haphazard page showing the most common HTML tags
-@app.route('/assignment-1')
+@app.route('/assignment1')
 def assignment_1():
     return redirect(url_for('static', filename='assignment-1.html'))
-
-@app.route('/assignment-2')
-def assignment_1():
-    return redirect(url_for('static', filename='assignment-2.html'))
-
-@app.route('/assignment-3')
-def assignment_1():
+# Assignment 1: A haphazard page showing the most common HTML tags
+@app.route('/assignment3')
+def assignment_3():
     return redirect(url_for('static', filename='assignment-3.html'))
 
-# A module runner to make our app go!
+
 if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
